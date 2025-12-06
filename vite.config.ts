0b15e0bf3a63771be22b4c9288ecc9e00af30b68
romain perset <romainperset@mac.home>
2025-12-06 18:14:36 +0100

    build: increase chunk size limit

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,9 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': path.resolve(__dirname, '.'),
       }
+    },
+    build: {
+      chunkSizeWarningLimit: 2000,
     }
   };
 });
